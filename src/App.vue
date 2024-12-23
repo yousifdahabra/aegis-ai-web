87b8e61234d0b6d2e42ac46efdeddb7c2f72a5e1
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-24 00:36:57 +0200

    Front-end - update app add login page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
-<template>
 
+<template>
+  <Login />
 </template>
 
 <script>
+import Login from './pages/Login';
 
 export default {
   name: 'App',
   components: {
-    
+    Login
   }
 }
 </script>
@@ -17,8 +19,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
