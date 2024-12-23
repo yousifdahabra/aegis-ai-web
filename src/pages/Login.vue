952735c4364693e66bf43d0030aacf2791e9d1c4
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-24 00:48:25 +0200

    Front-end - update login page start base

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,5 +1,12 @@
 <template>
-  
+    <div class="flex main-login">
+        <div class="login-form">
+            <input type="text" placeholder="email" name="" id="">
+        </div>
+        <div class="login-image">
+            <img src="https://cdn.mos.cms.futurecdn.net/SyDThccFHHMeSP2gbK8uHG-1024-80.jpg.webp" alt="">
+        </div>
+    </div>
 </template>
 
 <script>
