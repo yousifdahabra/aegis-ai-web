bf40a0e798a68103a47abddda3cf44403130b873
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-24 11:51:24 +0200

    Front-end - update Label_Input component add props

--- src/components/common/Label_Input.vue
+++ src/components/common/Label_Input.vue
@@ -3,12 +3,16 @@
         <label for="Email">Email</label>
         <input type="text" placeholder="Email" name="" id="">
     </div>
-
 </template>
 
 <script>
 export default {
-
+    props: {
+    label: String,
+    placeholder: String,
+    type: String,
+    modelValue: String,
+  } 
 }
 </script>
 
