534c5b28c0a4eecd993ccac224dde6fbadcf3909
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-24 17:55:16 +0200

    Front-end - update login page and style

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -6,16 +6,23 @@
                 <h1 class="main-title">Welcome Back!</h1>
                 <p class="sub-title">Enter your credentials to log in to your account</p>
             </div>
-            <div class="flex flex-direction-column ">
-                <form action="">
+            <form class="flex flex-direction-column main-form">
+
+                <div class="flex flex-direction-column">
                     <LabelInput label="Email" placeholder="Enter Your Email" type="email" v-model="email" />
                     <LabelInput label="Password" placeholder="Enter Your Password" type="password" v-model="password" />
                     <div class="flex flex-direction-row flex-wrap align-content-center justify-content-flex-start align-items-center form-check">
                         <input type="checkbox" v-model="rememberMe" >
                         <label for="Remember me" >Remember me</label>
                     </div>
-                </form>
-            </div>
+                </div>
+
+                <button class="main-btn">Login</button>
+
+                <p>
+                    Don't have an account? <a href="">Sign Up</a> 
+                </p>
+            </form>
         </div>
         <div class="login-image">
             <img src="https://cdn.mos.cms.futurecdn.net/SyDThccFHHMeSP2gbK8uHG-1024-80.jpg.webp" alt="">
