8dd99adba8a6d76004e9a88fa8b5c35ccf9bbff9
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-24 02:31:50 +0200

    Front-end - update login add form-header, form-input

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,7 +1,14 @@
 <template>
     <div class="flex flex-direction-row flex-wrap align-content-center justify-content-space-around align-items-center main-login">
         <div class="login-form">
-            <input type="text" placeholder="email" name="" id="">
+            <div class="flex flex-direction-column form-header">
+                <h1>Welcome Back!</h1>
+                <p>Enter your credentials to log in to your account</p>
+            </div>
+            <div class="flex flex-direction-column form-input">
+                <label for="">Email</label>
+                <input type="text" placeholder="email" name="" id="">
+            </div>
         </div>
         <div class="login-image">
             <img src="https://cdn.mos.cms.futurecdn.net/SyDThccFHHMeSP2gbK8uHG-1024-80.jpg.webp" alt="">
