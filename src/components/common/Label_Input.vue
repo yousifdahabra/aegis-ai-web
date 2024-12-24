9fc2b825c15cb8ac5005ddfefa33631245957570
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-24 11:49:50 +0200

    Front-end - update Label_Input component add label with input

--- src/components/common/Label_Input.vue
+++ src/components/common/Label_Input.vue
@@ -1,4 +1,8 @@
 <template>
+    <div class="flex flex-direction-column form-input">
+        <label for="Email">Email</label>
+        <input type="text" placeholder="Email" name="" id="">
+    </div>
 
 </template>
 
