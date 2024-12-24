6a99e3c50eb636364469d08fe55f237822ef3494
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-24 10:00:05 +0200

    Front-end - update main page set base style link

--- src/App.vue
+++ src/App.vue
@@ -14,17 +14,6 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-*{
-  margin: 0;
-  padding: 0;
-}
-</style>
 <style src="./assets/styles/common/flex.css"></style>
+<style src="./assets/styles/common/base.css"></style>
 <style src="./assets/styles/pages/login.css"></style>
