5fdab92228072290623daf535f39ddfef53ff815
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2024-12-24 11:54:55 +0200

    Front-end - update login page add LabelInput

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,28 +1,38 @@
 <template>
-    <div class="flex flex-direction-row flex-wrap align-content-center justify-content-space-around align-items-center main-login">
+    <div
+        class="flex flex-direction-row flex-wrap align-content-center justify-content-space-around align-items-center main-login">
         <div class="login-form">
             <div class="flex flex-direction-column form-header">
                 <h1 class="main-title">Welcome Back!</h1>
                 <p class="sub-title">Enter your credentials to log in to your account</p>
             </div>
             <div class="flex flex-direction-column ">
-                <div class="flex flex-direction-column form-input">
-                    <label for="Email">Email</label>
-                    <input type="text" placeholder="Email" name="" id="">
-                </div>
+                <form action="">
+
+                </form>
             </div>
         </div>
         <div class="login-image">
             <img src="https://cdn.mos.cms.futurecdn.net/SyDThccFHHMeSP2gbK8uHG-1024-80.jpg.webp" alt="">
         </div>
     </div>
+    
+
+    
+                
+
 </template>
 
 <script>
+import LabelInput from '@/components/common/Label_Input.vue'
+
 export default {
+    components: {
+        LabelInput
+    },
+
+    
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
