89b9821eea798145e40dcdc7d7e18f7b07bc95a3
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-14 19:02:38 +0200

    Front-end - update FeaturesSection ADD parallax animation

--- src/components/home/FeaturesSection.vue
+++ src/components/home/FeaturesSection.vue
@@ -1,24 +1,26 @@
 <template>
-  <section class="features">
-    <div class="container">
-      <h2 class="section-title">Why Choose Our App?</h2>
+  <section class="features-wrapper">
+    <div class="parallax-bg"></div>
+    <section class="features">
+      <div class="container">
+        <h2 class="section-title">Why Choose Our App?</h2>
 
-      <div
-        v-for="(feature, index) in features"
-        :key="index"
-        class="feature"
-        :class="{ reverse: index % 2 !== 0, 'is-active': index === activeFeatureIndex }"
-      >
-        <div class="feature-image-container">
-          <img :src="feature.image" :alt="feature.alt" class="feature-image" />
-        </div>
-        <div class="feature-text">
-          <h3>{{ feature.title }}</h3>
-          <p>{{ feature.description }}</p>
+        <div
+          v-for="(feature, index) in features"
+          :key="index"
+          class="feature"
+          :class="{ reverse: index % 2 !== 0, 'is-active': index === activeFeatureIndex }"
+        >
+          <div class="feature-image-container">
+            <img :src="feature.image" :alt="feature.alt" class="feature-image" />
+          </div>
+          <div class="feature-text">
+            <h3>{{ feature.title }}</h3>
+            <p>{{ feature.description }}</p>
+          </div>
         </div>
       </div>
-    </div>
-
+    </section>
   </section>
 </template>
 
@@ -56,7 +58,7 @@ export default {
 
     const handleScroll = () => {
       const featuresSection = document.querySelector(".features");
-      const { top, height } = featuresSection.getBoundingClientRect();
+      const { height } = featuresSection.getBoundingClientRect();
       const scrollPosition = window.scrollY - (featuresSection.offsetTop - window.innerHeight / 2);
 
       const featureHeight = height / features.length;
@@ -85,10 +87,30 @@ export default {
 };
 </script>
 <style scoped>
+/* Features Wrapper */
+.features-wrapper {
+  position: relative;
+  overflow: hidden;
+}
+
+.parallax-bg {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 150%;
+  background-image: url("@/assets/images/home/features_background.webp");
+  background-attachment: fixed;
+  background-size: cover;
+  background-position: center;
+  z-index: -1;
+}
+
 /* Features */
 .features {
   padding: 60px 20px;
-  background: #f9f9f9;
+  background: rgb(255 255 255 / 60%); /* Slight background overlay for readability */
+  position: relative; /* Ensure content stays above the parallax background */
 }
 
 .feature {
