38ddda6d79ab302e93215f27bd746d078525b7ba
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-14 13:15:09 +0200

    Front-end - update Header add burger-menu

--- src/components/home/Header.vue
+++ src/components/home/Header.vue
@@ -4,13 +4,21 @@
     <div class="container navbar-container">
       <div class="logo-nav-container">
         <img src="https://placehold.co/150x50" alt="Logo" class="logo" />
-        <nav class="nav-links" >
+        <nav class="nav-links"  >
           <a href="#" class="nav-item">Home</a>
           <a href="#" class="nav-item">About Us</a>
           <a href="#" class="nav-item">Contact Us</a>
           <a href="#" class="nav-item btn-expert-mobile">Be an Expert</a>
         </nav>
       </div>
+
+      <a href="#" class="btn-expert">Be an Expert</a>
+
+      <button class="burger-menu" >
+        <span class="line"></span>
+        <span class="line"></span>
+        <span class="line"></span>
+      </button>
     </div>
   </header>
 
