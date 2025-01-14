c81c900f92f7d4678905103eac51e527735ce718
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-14 16:28:52 +0200

    Front-end - add  components home HeroSection

---
+++ src/components/home/HeroSection.vue
@@ -0,0 +1,12 @@
+<template>
+    <!-- Hero Section -->
+    <section class="hero">
+    <div class="container text-center">
+    </div>
+  </section>
+
+</template>
+<script>
+</script>
+<style>
+</style>
