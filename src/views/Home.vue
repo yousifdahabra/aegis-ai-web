c888cb8086a50f0744e8ee55d8ea111e540f5292
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-14 16:31:43 +0200

    Front-end - update app add HeroSection

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="home-page">
     <CustomHeader />
-
+    <HeroSection />
   </div>
 </template>
 
 <script>
 import CustomHeader from "@/components/home/Header.vue";
+import HeroSection from "@/components/home/HeroSection.vue";
 
 export default {
   name: "Home",
   components: {
     CustomHeader,
+    HeroSection,
   },
 
   data() {
