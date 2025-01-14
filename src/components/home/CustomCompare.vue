28ff017482f1cfadcbbad0f9ca35066751d16214
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-14 22:53:47 +0200

    Front-end - add CustomCompare

---
+++ src/components/home/CustomCompare.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="timeline-section">
+    <div class="container">
+      <h2 class="section-title">How the Test Works</h2>
+      <div class="timeline">
+
+
+
+      </div>
+    </div>
+  </section>
+</template>
+
+
+
+<script>
+export default {
+  name: "CustomCompare",
+};
+</script>
+<style scoped>
+ </style>
