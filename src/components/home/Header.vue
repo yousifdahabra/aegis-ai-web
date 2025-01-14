c8d156de7dcb25582311b0d0f61ced5d689b380d
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-14 13:16:19 +0200

    Front-end - update Header add toggleMenu

--- src/components/home/Header.vue
+++ src/components/home/Header.vue
@@ -4,17 +4,16 @@
     <div class="container navbar-container">
       <div class="logo-nav-container">
         <img src="https://placehold.co/150x50" alt="Logo" class="logo" />
-        <nav class="nav-links"  >
+        <nav class="nav-links" :class="{ 'show-menu': isMenuOpen }">
           <a href="#" class="nav-item">Home</a>
           <a href="#" class="nav-item">About Us</a>
           <a href="#" class="nav-item">Contact Us</a>
           <a href="#" class="nav-item btn-expert-mobile">Be an Expert</a>
         </nav>
       </div>
-
       <a href="#" class="btn-expert">Be an Expert</a>
 
-      <button class="burger-menu" >
+      <button class="burger-menu" @click="toggleMenu">
         <span class="line"></span>
         <span class="line"></span>
         <span class="line"></span>
@@ -28,7 +27,16 @@
 <script>
 export default {
   name: "CustomHeader",
-  setup() { },
+  data() {
+    return {
+      isMenuOpen: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+    },
+  },
 };
 </script>
 
