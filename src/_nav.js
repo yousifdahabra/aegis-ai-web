b6b03d88959f3a8e6e6ba3aa54bba3f2a8df04ce
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-16 18:15:33 +0200

    Front-end - update sidebar add Users

--- src/_nav.js
+++ src/_nav.js
@@ -6,4 +6,27 @@ export default [
     icon: 'cil-speedometer',
     role: 'all',
   },
+  {
+    component: 'CNavGroup',
+    name: 'Users',
+    to: '/base/users',
+    icon: 'cil-people',
+    role: 'admin',
+    items: [
+      {
+        component: 'CNavItem',
+        name: 'Add User',
+        to: '/base/users/add',
+      },
+      {
+        component: 'CNavItem',
+        name: 'List Users',
+        to: '/base/users/list',
+      },
+    ],
+  },
+
+
+
+
 ];
