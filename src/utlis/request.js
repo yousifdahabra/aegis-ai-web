d4dca06c96d642d76899cc3a5d8c2759803077ed
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-16 12:37:52 +0200

    Front-end - add utlis request

---
+++ src/utlis/request.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+axios.defaults.baseURL = "http://localhost/learning/back/";
+
+export const requestAPI =  async({route,method = 'GET' ,body,header='application/x-www-form-urlencoded'}) =>{
+
+
+
+}
