bc1ffef95e11975d4d81233b90dd3414d29099e9
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-16 18:41:12 +0200

    Front-end - update UsersList add CTableHead

--- src/views/base/users/UsersList.vue
+++ src/views/base/users/UsersList.vue
@@ -2,6 +2,14 @@
   <div>
     <h1>Users List</h1>
     <CTable striped hover>
+      <CTableHead>
+        <CTableRow>
+          <CTableHeaderCell scope="col">Name</CTableHeaderCell>
+          <CTableHeaderCell scope="col">Email</CTableHeaderCell>
+          <CTableHeaderCell scope="col">Total Tests</CTableHeaderCell>
+          <CTableHeaderCell scope="col">Actions</CTableHeaderCell>
+        </CTableRow>
+      </CTableHead>
     </CTable>
   </div>
 </template>
