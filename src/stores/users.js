22a1679c439dd5372cb798d18a8108aa0ba472b5
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-16 19:16:58 +0200

    Front-end - update users update fetchUsers add response

--- src/stores/users.js
+++ src/stores/users.js
@@ -8,11 +8,23 @@ export const useUsersStore = defineStore('users', {
   }),
   actions: {
     async fetchUsers() {
+      try {
         const response = await requestAPI({
           route: 'users',
           method: 'GET',
         });
 
+        if (response.status) {
+          this.users = response.data;
+          return { success: true };
+        } else {
+          this.errorMessage = response.message;
+          return { success: false, message: response.message };
+        }
+      } catch (error) {
+        this.errorMessage = 'An error occurred while fetching users.';
+        return { success: false, message: 'An error occurred while fetching users.' };
+      }
     },
   },
 });
