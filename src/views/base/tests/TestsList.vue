e2fdb08b8ced6c73a87464d257ff726cf8f699d7
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-17 01:54:15 +0200

    Front-end - update TestsList add CTableHead

--- src/views/base/tests/TestsList.vue
+++ src/views/base/tests/TestsList.vue
@@ -5,6 +5,17 @@
     <div v-if="successMessage" class="text-success mb-3">{{ successMessage }}</div>
     <div v-if="loading" class="text-center mb-3">Loading...</div>
     <CTable striped hover v-if="!loading && tests.length">
+      <CTableHead>
+        <CTableRow>
+          <CTableHeaderCell>#</CTableHeaderCell>
+          <CTableHeaderCell>Title</CTableHeaderCell>
+          <CTableHeaderCell>Security</CTableHeaderCell>
+          <CTableHeaderCell>User Name</CTableHeaderCell>
+          <CTableHeaderCell>State</CTableHeaderCell>
+          <CTableHeaderCell>Questions Count</CTableHeaderCell>
+          <CTableHeaderCell>Actions</CTableHeaderCell>
+        </CTableRow>
+      </CTableHead>
     </CTable>
   </div>
 </template>
