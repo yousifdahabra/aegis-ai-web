01ae6264d85fae5f3dc015f911614402dbef6d71
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-16 12:55:56 +0200

    Front-end - update auth.js add useAuthStore

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,2 +1,17 @@
 import { defineStore } from 'pinia';
 import { requestAPI } from '@/utils/request';
+
+export const useAuthStore = defineStore('auth', {
+  state: () => ({}),
+  actions: {
+    async login(credentials) {
+        const response = await requestAPI({
+          route: 'login',
+          method: 'POST',
+          body: credentials,
+          header: 'application/json',
+        });
+    },
+  },
+  getters: {},
+});
