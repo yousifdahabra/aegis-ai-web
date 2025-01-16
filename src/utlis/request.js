f1f3eab852c2cc642ff60133efa18bdd05bbc833
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-16 12:38:04 +0200

    Front-end - update request add axios

--- src/utlis/request.js
+++ src/utlis/request.js
@@ -3,7 +3,22 @@ import axios from 'axios'
 axios.defaults.baseURL = "http://localhost/learning/back/";
 
 export const requestAPI =  async({route,method = 'GET' ,body,header='application/x-www-form-urlencoded'}) =>{
+    try{
+        const respons = await axios.request({
+            url:`${route}.php`,
+            method,
+            data:body,
+            headers:{
+                'Content-Type': header,
+                // "Content-Type":"application/json",  application/x-www-form-urlencoded
+                Authorization:localStorage.token,
+            }
+        });
+        return respons.data;
 
+    }catch (error){
+        return error;
+    }
 
 
 }
