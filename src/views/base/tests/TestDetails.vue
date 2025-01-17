e948fc1517d4bef5d184aed38552a06aa0c02b9d
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-17 03:46:46 +0200

    Front-end - update TestDetails add CCardBody

---
+++ src/views/base/tests/TestDetails.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1 class="mb-4">Test Details</h1>
+    <CContainer>
+      <!-- Test Overview -->
+      <CCard class="mb-4">
+        <CCardHeader>Test Overview</CCardHeader>
+        <CCardBody v-if="testDetails">
+          <p><strong>Test ID:</strong> {{ testDetails.id }}</p>
+          <p><strong>Title:</strong> {{ testDetails.title }}</p>
+          <p><strong>Security:</strong> {{ testDetails.security }}</p>
+          <p><strong>User Name:</strong> {{ testDetails.user_name }}</p>
+          <p><strong>State:</strong> {{ testDetails.test_state }}</p>
+        </CCardBody>
+        <CCardBody v-else>
+          <div class="text-center">Loading test details...</div>
+        </CCardBody>
+      </CCard>
+
+
+
+    </CContainer>
+  </div>
+</template>
+
+<script>
+</script>
