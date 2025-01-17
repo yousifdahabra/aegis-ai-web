90f8e7d06e0fe6bc362cd4ef36572fe69a82aa79
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-18 01:38:00 +0200

    Front-end - add CTableBody

--- src/views/base/expert/ExpertRequestList.vue
+++ src/views/base/expert/ExpertRequestList.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <h1>Expert Request List</h1>
+    <div v-if="errorMessage" class="text-danger mb-3">{{ errorMessage }}</div>
+    <div v-if="successMessage" class="text-success mb-3">{{ successMessage }}</div>
+    <div v-if="loading" class="text-center mb-3">Loading...</div>
 
     <CTable striped hover v-if="!loading && expertRequests.length">
       <CTableHead>
@@ -11,6 +14,31 @@
           <CTableHeaderCell>Files</CTableHeaderCell>
         </CTableRow>
       </CTableHead>
+      <CTableBody>
+        <CTableRow v-for="request in expertRequests" :key="request.id">
+          <CTableDataCell>{{ request.user?.name || "N/A" }}</CTableDataCell>
+          <CTableDataCell>{{ request.user?.email || "N/A" }}</CTableDataCell>
+          <CTableDataCell>{{ request.extra_informations || "N/A" }}</CTableDataCell>
+          <CTableDataCell>
+            <div v-if="request.expert_request_documents?.length > 0">
+              <div
+                v-for="(doc, index) in request.expert_request_documents"
+                :key="index"
+                class="mb-2"
+              >
+                <CButton
+                  color="info"
+                  size="sm"
+                  @click="downloadDocument(doc.file_path)"
+                >
+                  Download File {{ index + 1 }}
+                </CButton>
+              </div>
+            </div>
+            <div v-else>No files available</div>
+          </CTableDataCell>
+        </CTableRow>
+      </CTableBody>
     </CTable>
 
   </div>
