56689e1492481b6d2cf4f5feb1617f255ea8f05e
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-17 02:06:33 +0200

    Front-end - update useTestsStore add fetchTests

--- src/stores/tests.js
+++ src/stores/tests.js
@@ -1,2 +1,36 @@
 import { defineStore } from 'pinia';
 import { requestAPI } from '@/utlis/request';
+
+export const useTestsStore = defineStore('tests', {
+  state: () => ({
+    tests: [],
+    loading: false,
+    errorMessage: null,
+    successMessage: null,
+  }),
+  actions: {
+    async fetchTests(userId) {
+      try {
+        this.loading = true;
+        this.errorMessage = null;
+
+        const response = await requestAPI({
+          route: `tests/get-user-list/${userId}`,
+          method: 'GET',
+          header: 'application/json',
+        });
+
+        if (response.success) {
+          this.tests = response.data;
+        } else {
+          this.errorMessage = response.message;
+          this.tests = [];
+        }
+      } catch (error) {
+        this.errorMessage = 'Failed to fetch tests. Please try again.';
+      } finally {
+        this.loading = false;
+      }
+    },
+  },
+});
