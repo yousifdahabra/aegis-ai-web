53b49fc95af8be4cc67454fb6570b8edb791c6d7
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-18 01:36:57 +0200

    Front-end - add Expert Request List

---
+++ src/views/base/expert/ExpertRequestList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Expert Request List</h1>
+
+    <CTable striped hover v-if="!loading && expertRequests.length">
+      <CTableHead>
+        <CTableRow>
+          <CTableHeaderCell>User Name</CTableHeaderCell>
+          <CTableHeaderCell>Email</CTableHeaderCell>
+          <CTableHeaderCell>Extra Information</CTableHeaderCell>
+          <CTableHeaderCell>Files</CTableHeaderCell>
+        </CTableRow>
+      </CTableHead>
+    </CTable>
+
+  </div>
+</template>
+
+<script>
+</script>
