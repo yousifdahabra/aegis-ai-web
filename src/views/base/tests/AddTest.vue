dd45a89ef348e73e91148ee12edab96fdb50ce80
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-17 16:46:36 +0200

    Front-end - update AddTest add question

--- src/views/base/tests/AddTest.vue
+++ src/views/base/tests/AddTest.vue
@@ -9,12 +9,38 @@
         required
       />
     </div>
+    <div v-for="(question, index) in questions" :key="index" class="mb-4">
+      <h5>Question {{ index + 1 }}</h5>
+      <CFormInput
+        v-model="question.text"
+        label="Question Text"
+        placeholder="Enter question text"
+        required
+        class="mb-2"
+      />
+      <CFormSelect
+        v-model="question.type"
+        label="Question Type"
+        :options="questionTypes"
+        required
+        class="mb-2"
+      />
 
+
+  </div>
 </template>
 
 <script>
 </script>
 
-<style >
-
+<style scoped>
+.d-flex {
+  display: flex;
+}
+.justify-content-between {
+  justify-content: space-between;
+}
+.align-items-center {
+  align-items: center;
+}
 </style>
