0c93f1c72c635648afb977617c7ee31370d4512e
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-17 16:45:52 +0200

    Front-end - add Add Test

---
+++ src/views/base/tests/AddTest.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Add Test</h1>
+    <div class="mb-4">
+      <CFormInput
+        v-model="testTitle"
+        label="Test Title"
+        placeholder="Enter the test title"
+        required
+      />
+    </div>
+
+</template>
+
+<script>
+</script>
+
+<style >
+
+</style>
