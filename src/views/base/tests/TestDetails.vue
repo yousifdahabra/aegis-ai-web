29431665d6231640b9bcdc521d6f7c0e9c254760
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-17 03:47:27 +0200

    Front-end - update TestDetails add Questions and Answers

--- src/views/base/tests/TestDetails.vue
+++ src/views/base/tests/TestDetails.vue
@@ -2,7 +2,6 @@
   <div>
     <h1 class="mb-4">Test Details</h1>
     <CContainer>
-      <!-- Test Overview -->
       <CCard class="mb-4">
         <CCardHeader>Test Overview</CCardHeader>
         <CCardBody v-if="testDetails">
@@ -17,7 +16,28 @@
         </CCardBody>
       </CCard>
 
-
+      <CCard class="mb-4">
+        <CCardHeader>Questions and Answers</CCardHeader>
+        <CCardBody>
+          <CTable striped hover v-if="questions.length">
+            <CTableHead>
+              <CTableRow>
+                <CTableHeaderCell>#</CTableHeaderCell>
+                <CTableHeaderCell>Question</CTableHeaderCell>
+                <CTableHeaderCell>Answer</CTableHeaderCell>
+              </CTableRow>
+            </CTableHead>
+            <CTableBody>
+              <CTableRow v-for="(question, index) in questions" :key="question.id">
+                <CTableDataCell>{{ index + 1 }}</CTableDataCell>
+                <CTableDataCell>{{ question.title }}</CTableDataCell>
+                <CTableDataCell>{{ question.option_answer || 'No answer provided' }}</CTableDataCell>
+              </CTableRow>
+            </CTableBody>
+          </CTable>
+          <div v-else class="text-center">No questions found.</div>
+        </CCardBody>
+      </CCard>
 
     </CContainer>
   </div>
