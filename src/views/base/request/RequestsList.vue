c0374a4f59f8d33f9ca277a766ded6b20c55b4f7
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-17 15:26:32 +0200

    Front-end - add RequestsList

---
+++ src/views/base/request/RequestsList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>View Requests</h1>
+    <div v-if="errorMessage" class="text-danger mb-3">{{ errorMessage }}</div>
+    <div v-if="successMessage" class="text-success mb-3">{{ successMessage }}</div>
+    <div v-if="loading" class="text-center mb-3">Loading...</div>
+    <CTable striped hover v-if="!loading && requests.length">
+      <CTableHead>
+        <CTableRow>
+          <CTableHeaderCell>User Name</CTableHeaderCell>
+          <CTableHeaderCell>About User</CTableHeaderCell>
+          <CTableHeaderCell>User Note</CTableHeaderCell>
+          <CTableHeaderCell>Links</CTableHeaderCell>
+          <CTableHeaderCell>Actions</CTableHeaderCell>
+        </CTableRow>
+      </CTableHead>
+    </CTable>
+  </div>
+</template>
+
+<script>
+</script>
