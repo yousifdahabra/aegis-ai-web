b3f49d13acf35e8d5511b6ce592a46000af373e7
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-18 02:22:44 +0200

    Front-end - update AppSidebar

--- src/components/AppSidebar.vue
+++ src/components/AppSidebar.vue
@@ -1,8 +1,6 @@
 <script setup>
 import { RouterLink } from 'vue-router'
 
-import { logo } from '@/assets/brand/logo'
-import { sygnet } from '@/assets/brand/sygnet'
 import { AppSidebarNav } from '@/components/AppSidebarNav.js'
 import { useSidebarStore } from '@/stores/sidebar.js'
 
@@ -21,8 +19,11 @@ const sidebar = useSidebarStore()
     <CSidebarHeader class="border-bottom">
       <RouterLink custom to="/" v-slot="{ href, navigate }">
         <CSidebarBrand v-bind="$attrs" as="a" :href="href" @click="navigate">
-          <CIcon custom-class-name="sidebar-brand-full" :icon="logo" :height="32" />
-          <CIcon custom-class-name="sidebar-brand-narrow" :icon="sygnet" :height="32" />
+          <h2 class="sidebar-brand-text">
+            <img class="sidebar-brand-full me-2" src="@/assets/brand/logo.png" alt="Logo" height="60"
+            />
+          AegisAI
+        </h2>
         </CSidebarBrand>
       </RouterLink>
       <CCloseButton class="d-lg-none" dark @click="sidebar.toggleVisible()" />
