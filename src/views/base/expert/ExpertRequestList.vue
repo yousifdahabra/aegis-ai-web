a2a565c0f0757d7a75f60b6413bf01b4c984b3d1
Yousif Mohammed Dahabra <yousif.dahabra.97@gmail.com>
2025-01-19 15:53:06 +0200

    Front-end - update Expert Request List

--- src/views/base/expert/ExpertRequestList.vue
+++ src/views/base/expert/ExpertRequestList.vue
@@ -7,67 +7,73 @@
     <div v-if="successMessage">
       <CAlert color="success" class="mb-3">{{ successMessage }}</CAlert>
     </div>
-    <div v-if="loading" class="text-center mb-3">Loading...</div>
 
-    <CTable striped hover v-if="!loading && expertRequests.length">
-      <CTableHead>
-        <CTableRow>
-          <CTableHeaderCell>User Name</CTableHeaderCell>
-          <CTableHeaderCell>Email</CTableHeaderCell>
-          <CTableHeaderCell>Extra Information</CTableHeaderCell>
-          <CTableHeaderCell>Files</CTableHeaderCell>
-          <CTableHeaderCell>Actions</CTableHeaderCell>
-        </CTableRow>
-      </CTableHead>
-      <CTableBody>
-        <CTableRow v-for="request in expertRequests" :key="request.id">
-          <CTableDataCell>{{ request.user?.name || "N/A" }}</CTableDataCell>
-          <CTableDataCell>{{ request.user?.email || "N/A" }}</CTableDataCell>
-          <CTableDataCell>{{ request.extra_informations || "N/A" }}</CTableDataCell>
-          <CTableDataCell>
-            <div v-if="request.expert_request_documents?.length > 0">
-              <div
-                v-for="(doc, index) in request.expert_request_documents"
-                :key="index"
-                class="mb-2"
-              >
-                <CButton
-                  color="info"
-                  size="sm"
-                  @click="downloadDocument(doc.file_path)"
-                >
-                  Download File {{ index + 1 }}
-                </CButton>
-              </div>
-            </div>
-            <div v-else>No files available</div>
-          </CTableDataCell>
-          <CTableDataCell>
-            <CButton
-              color="success"
-              size="sm"
-              class="me-2"
-              @click="acceptRequest(request.id)"
-            >
-              Accept
-            </CButton>
-            <CButton
-              color="danger"
-              size="sm"
-              @click="rejectRequest(request.id)"
-            >
-              Reject
-            </CButton>
-          </CTableDataCell>
-        </CTableRow>
-      </CTableBody>
-    </CTable>
-
-    <div v-else-if="!loading && !expertRequests.length" class="text-center">
-      No expert requests found.
+    <div>
+      <div v-if="loading" class="text-center mb-3">Loading...</div>
+      <div v-else-if="!loading && expertRequests.length" class="table-responsive">
+        <CTable striped hover>
+          <CTableHead>
+            <CTableRow>
+              <CTableHeaderCell>User Name</CTableHeaderCell>
+              <CTableHeaderCell>Email</CTableHeaderCell>
+              <CTableHeaderCell>Extra Information</CTableHeaderCell>
+              <CTableHeaderCell>Files</CTableHeaderCell>
+              <CTableHeaderCell>Actions</CTableHeaderCell>
+            </CTableRow>
+          </CTableHead>
+          <CTableBody>
+            <CTableRow v-for="request in expertRequests" :key="request.id">
+              <CTableDataCell>{{ request.user?.name || "N/A" }}</CTableDataCell>
+              <CTableDataCell>{{ request.user?.email || "N/A" }}</CTableDataCell>
+              <CTableDataCell class="text-wrap">{{ request.extra_informations || "N/A" }}</CTableDataCell>
+              <CTableDataCell>
+                <div v-if="request.expert_request_documents?.length > 0">
+                  <div
+                    v-for="(doc, index) in request.expert_request_documents"
+                    :key="index"
+                    class="mb-2"
+                  >
+                    <CButton
+                      color="info"
+                      size="sm"
+                      @click="downloadDocument(doc.file_path)"
+                    >
+                      Download File {{ index + 1 }}
+                    </CButton>
+                  </div>
+                </div>
+                <div v-else>No files available</div>
+              </CTableDataCell>
+              <CTableDataCell>
+                <div class="d-flex flex-column">
+                  <CButton
+                    color="success"
+                    size="sm"
+                    class="mb-2"
+                    @click="acceptRequest(request.id)"
+                  >
+                    Accept
+                  </CButton>
+                  <CButton
+                    color="danger"
+                    size="sm"
+                    @click="rejectRequest(request.id)"
+                  >
+                    Reject
+                  </CButton>
+                </div>
+              </CTableDataCell>
+            </CTableRow>
+          </CTableBody>
+        </CTable>
+      </div>
+      <div v-else class="text-center">
+        No expert requests found.
+      </div>
     </div>
   </div>
 </template>
+
 
 <script>
 import { useExpertRequestsStore } from "@/stores/expertRequests";
@@ -118,3 +124,13 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.table-responsive {
+  overflow-x: auto;
+}
+.text-wrap {
+  word-wrap: break-word;
+  white-space: normal;
+}
+</style>
